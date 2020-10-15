c54d850d65242bb6ec9c9807953313ac67ce7ca4
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2020-10-15 12:49:48 +0200

    Cylinder takeon out

--- Source/ModuleRenderer3D.cpp
+++ Source/ModuleRenderer3D.cpp
@@ -257,21 +257,7 @@ void ModuleRenderer3D::DrawPyramid()
 
 void ModuleRenderer3D::DrawCylinder()
 {
-	float radius, halfLength;
-	int slices;
-	for (int i = 0; i < slices; i++) {
-		float theta = ((float)i) * 2.0 * M_PI;
-		float nextTheta = ((float)i + 1) * 2.0 * M_PI;
-		glBegin(GL_TRIANGLE_STRIP);
-		/*vertex at middle of end */ glVertex3f(0.0, halfLength, 0.0);
-		/*vertices at edges of circle*/ glVertex3f(radius * cos(theta), halfLength, radius * sin(theta));
-		glVertex3f(radius * cos(nextTheta), halfLength, radius * sin(nextTheta));
-		/* the same vertices at the bottom of the cylinder*/
-		glVertex3f(radius * cos(nextTheta), -halfLength, radius * sin(nextTheta));
-		glVertex3f(radius * cos(theta), -halfLength, radius * sin(theta));
-		glVertex3f(0.0, -halfLength, 0.0);
-		glEnd();
-	}
+	
 }
 
 // Draw mesh with vertex and index
