2c729c11074f3f03bc08b0f0e5a0fd591eaf7ead
MarcArizaAlborni <45202074+MarcArizaAlborni@users.noreply.github.com>
2020-10-28 15:47:54 +0100

    small fix textures

--- Source/ModuleGeometryManager.cpp
+++ Source/ModuleGeometryManager.cpp
@@ -51,8 +51,8 @@ update_status ModuleGeometryManager::Update(float dt)
 		GameObject* Item = *IteratorLoaded;
 		//right now we only load the house texture
 		if (Item->is_Drawn == true) {
-			//DrawTextureOnMesh(*IteratorLoaded, App->textureImporter->mTextureID);
-			DrawMesh(*IteratorLoaded);
+			DrawTextureOnMesh(*IteratorLoaded, App->textureImporter->mTextureID);
+			//DrawMesh(*IteratorLoaded);
 		}
 		//DrawMesh(*IteratorLoaded);
 
