af214e4e3613f4c4164a60b83cae8f2daa92ba0c
MarcArizaAlborni <45202074+MarcArizaAlborni@users.noreply.github.com>
2020-12-01 16:04:11 +0100

    Update ModuleRenderer3D.cpp

--- Source/ModuleRenderer3D.cpp
+++ Source/ModuleRenderer3D.cpp
@@ -152,12 +152,13 @@ void ModuleRenderer3D::GenerateTextBuffer(float* text_coords, uint& num_text_coo
 void ModuleRenderer3D::GenerateNormalBuffer(GameObject* mesh, Vertex_Sub& normals)
 {
 	if (mesh->MeshData.normals != nullptr) {
+		if (mesh->MeshData.id_normals != NULL) {
 
-
-		uint val = 0;
-		glGenBuffers(1, (GLuint*)&(mesh->MeshData.id_normals));
-		glBindBuffer(GL_ARRAY_BUFFER, (GLuint) & (mesh->MeshData.id_normals));
-		glBufferData(GL_ARRAY_BUFFER, sizeof(uint) * mesh->MeshData.id_normals * 3, mesh->MeshData.normals, GL_STATIC_DRAW);
+			uint val = 0;
+			glGenBuffers(1, (GLuint*)&(mesh->MeshData.id_normals));
+			glBindBuffer(GL_ARRAY_BUFFER, (GLuint) & (mesh->MeshData.id_normals));
+			glBufferData(GL_ARRAY_BUFFER, sizeof(uint) * mesh->MeshData.id_normals * 3, mesh->MeshData.normals, GL_STATIC_DRAW);
+		}
 
 	}
 
