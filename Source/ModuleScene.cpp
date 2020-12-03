562d52920e7f9dd8168281f32f9696c6fde0ea7b
MarcArizaAlborni <45202074+MarcArizaAlborni@users.noreply.github.com>
2020-12-03 19:38:04 +0100

    Update ModuleScene.cpp

--- Source/ModuleScene.cpp
+++ Source/ModuleScene.cpp
@@ -66,6 +66,10 @@ bool ModuleScene::Start()
 	//App->meshimporter->LoadFile_Mesh("Assets/Models/House/BakerHouse.fbx");
 	//this
 	App->meshimporter->LoadFile_Mesh("Assets/Models/Street/Street_environment_V01.FBX");
+
+	
+
+
 	//App->meshimporter->LoadFile_Mesh("Assets/Models/Street/Street_environment_V01.FBX");
 	//App->meshimporter->LoadFile_Mesh("Assets/Models/Street/Street_environment.FBX");
 
